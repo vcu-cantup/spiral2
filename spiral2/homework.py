87992e3a4079e6c339fc2fe2fa0b1e639f1952d9
vcu-cantup <78399430+vcu-cantup@users.noreply.github.com>
2021-05-03 22:00:36 -0400

    Add files via upload

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,6 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
+    size = (size-1)/2
+    n = 2*size*(8*size**2+15*size+13)/3+1
     return_value = n
     return return_value
+print(spiralize(501))
